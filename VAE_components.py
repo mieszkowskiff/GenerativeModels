Refactor VAE loss function to fix KLD calculation

VAE_components.py
@@ -140,7 +140,7 @@ class VAE(torch.nn.Module):
 
 def vae_loss_function(recon_x, x, mu, logvar, split = False, alpha = 0.0001, beta = 0.01):
     MSE = torch.nn.MSELoss(reduction='sum')(recon_x, x)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) / x.shape[0]
+    KLD = -0.5 * torch.sum( 1 - mu.pow(2) + logvar - logvar.exp()) / x.shape[0]
     if split:
         return MSE, KLD
     return alpha * MSE + beta * KLD

analyze.py
@@ -4,7 +4,7 @@ from torchvision import transforms, datasets
 import tqdm
 from utils import display
 def main():
-    torch.manual_seed(13)
+    torch.manual_seed(10)
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -52,24 +52,24 @@ def main():
 
     mus = torch.tensor([])
     encoder.eval()
-    with torch.no_grad():
-        for data, _ in tqdm.tqdm(dataloader):
-            data = data.to(device)
-            mu, logvar = encoder(data)
-            mus = torch.cat((mus, mu.to("cpu")), dim=0)
-    mean = mus.mean(dim=0)  # shape: [256]
+    # with torch.no_grad():
+    #     for data, _ in tqdm.tqdm(dataloader):
+    #         data = data.to(device)
+    #         mu, logvar = encoder(data)
+    #         mus = torch.cat((mus, mu.to("cpu")), dim=0)
+    # mean = mus.mean(dim=0)  # shape: [256]
 
-    # 2. Centrowanie danych
-    mus_centered = mus - mean
+    # # 2. Centrowanie danych
+    # mus_centered = mus - mean
 
-    # 3. Macierz kowariancji próbkowej
-    N = mus.shape[0]
-    cov = (mus_centered.T @ mus_centered) / (N - 1)
+    # # 3. Macierz kowariancji próbkowej
+    # N = mus.shape[0]
+    # cov = (mus_centered.T @ mus_centered) / (N - 1)
 
-    eps = 1e-4
-    cov_regularized = cov + eps * torch.eye(cov.shape[0], device=cov.device)
-    L = torch.linalg.cholesky(cov_regularized).to(device)
-    mean = mean.to(device)
+    # eps = 1e-4
+    # cov_regularized = cov + eps * torch.eye(cov.shape[0], device=cov.device)
+    # L = torch.linalg.cholesky(cov_regularized).to(device)
+    # mean = mean.to(device)
 
 
 
@@ -78,7 +78,7 @@ def main():
         for i in range(100):
 
             e = torch.randn(256).to(device)
-            z = L @ e + mean
+            z = e
             z = z.view(-1, 256)
 
             display(decoder(z))

train_VAE.py
@@ -7,7 +7,7 @@ from torchsummary import summary
 from utils import display
 
 def main():
-    torch.manual_seed(13)
+    torch.manual_seed(12)
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -61,7 +61,7 @@ def main():
             print(f"Epoch {epoch + 1}/{num_epochs}, MSE_loss: {MSE_loss:.4f}, KLD_loss: {KLD_loss:.4f}")
     
     # Save the model
-    torch.save(model.state_dict(), "autoencoder2.pth")
+    torch.save(model.state_dict(), "autoencoder3.pth")
     with torch.no_grad():
         model.eval()
         for i in range(10):