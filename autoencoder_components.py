3ee5d09557e63620b9fef0e78bbec502fa59a34f
mieszkowskiff <mieszkowskiff@gmail.com>
2025-05-16 17:00:45 +0200

    Add autoencoder components and implement DiffusionAutoencoder with beta and alpha parameters

---
+++ autoencoder_components.py
@@ -0,0 +1,134 @@
+import torch.nn as nn
+
+
+
+class EncoderConvBlock(nn.Module):
+    def __init__(self, channels):
+        super(EncoderConvBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, kernel_size = 3, padding = 1)
+        self.bn = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        y = self.conv(x)
+        y = self.bn(y)
+        return nn.ReLU()(y + x)
+    
+
+class Encoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Encoder, self).__init__()
+        self.latent_dim = latent_dim
+
+        self.starting_conv = nn.Conv2d(
+            in_channels = 3, 
+            out_channels = 64, 
+            kernel_size = 3, 
+            padding = 1
+        )
+
+        self.conv = nn.Sequential(
+            EncoderConvBlock(64),
+            EncoderConvBlock(64),
+            EncoderConvBlock(64),
+            EncoderConvBlock(64),
+        )
+
+        self.connection_conv = nn.Conv2d(
+            in_channels = 64, 
+            out_channels = 4, 
+            kernel_size = 3, 
+            padding = 1
+        )
+
+
+        self.dense = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(32 * 32 * 4, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, latent_dim),
+            nn.ReLU(),
+        )
+    def forward(self, x):
+        x = self.starting_conv(x)
+        x = self.conv(x)
+
+        x = self.connection_conv(x)
+        x = nn.MaxPool2d(kernel_size = 2)(x)
+        x = nn.ReLU()(x)
+        x = self.dense(x)
+        return x
+
+
+class DecoderConvBlock(nn.Module):
+    def __init__(self, channels):
+        super(DecoderConvBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, kernel_size = 3, padding = 1)
+        self.bn = nn.BatchNorm2d(channels)
+    
+    def forward(self, x):
+        y = self.conv(x)
+        y = self.bn(y)
+        return nn.ReLU()(x + y)
+    
+class Decoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(Decoder, self).__init__()
+        self.latent_dim = latent_dim
+
+        self.dense = nn.Sequential(
+            nn.Linear(latent_dim, 512),
+            nn.ReLU(),
+            nn.Linear(512, 2048),
+            nn.ReLU(),
+            nn.Linear(2048, 8192),
+            nn.ReLU(),
+        )
+
+        self.connection_conv = nn.Conv2d(
+            in_channels = 2, 
+            out_channels = 128, 
+            kernel_size = 3, 
+            padding = 1
+        )
+
+        self.conv = nn.Sequential(
+            EncoderConvBlock(128),
+            EncoderConvBlock(128),
+            EncoderConvBlock(128),
+            EncoderConvBlock(128),
+        )
+
+        self.ending_conv = nn.Conv2d(
+            in_channels = 128, 
+            out_channels = 3, 
+            kernel_size = 3, 
+            padding = 1
+        )
+
+    def forward(self, z):
+        z = self.dense(z)
+        z = z.view(-1, 2, 64, 64)
+        z = self.connection_conv(z)
+        z = nn.ReLU()(z)
+        z = self.conv(z)
+        z = self.ending_conv(z)
+        return z
+        
+    
+
+class AutoEncoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(AutoEncoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.latent_dim = encoder.latent_dim
+        assert self.latent_dim == decoder.latent_dim, "Encoder and decoder latent dimensions must match."
+
+
+
+    def forward(self, x):
+        z = self.encoder(x)
+        y = self.decoder(z)
+        return y, z
--- components.py
+++ components.py
@@ -1,1 +1,15 @@
-import torch
+import autoencoder_components
+
+
+
+class DiffusionAutoencoder(autoencoder_components.AutoEncoder):
+    def __init__(self, latent_dim, beta_min = 0.001, beta_max = 0.02, steps = 1000):
+        super(DiffusionAutoencoder, self).__init__(latent_dim)
+        self.beta = list(range(beta_min, beta_max, (beta_max - beta_min) / steps))
+        self.alpha = [1 - b for b in self.beta]
+        self.alpha_hat = [1]
+        for i in range(1, len(self.alpha)):
+            self.alpha_hat.append(self.alpha_hat[i - 1] * self.alpha[i])
+
+
+        
