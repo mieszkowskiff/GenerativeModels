dd7f4ff9c968ba9c1f9569e2081b8fdd125429a0
mieszkowskiff <mieszkowskiff@gmail.com>
2025-05-16 16:51:00 +0200

    Add import statement for torch in components.py

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ from torch.utils.data import DataLoader
 
 def main():
     transform = transforms.ToTensor()
-    mnist_train = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    subset_indices = torch.where(mnist_train.targets == 7)[0]
-    mnist_subset = torch.utils.data.Subset(mnist_train, subset_indices)
-    subset_loader = DataLoader(mnist_subset, batch_size=64, shuffle=True)
-
-    for images, _ in subset_loader:
-        print(images.shape)
-
+    
 
 
 
