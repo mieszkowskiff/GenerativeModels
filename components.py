897286686e6d26a2ef9d376267719e79adfef9c4
mieszkowskiff <mieszkowskiff@gmail.com>
2025-05-27 23:51:42 +0200

    Refactor main.py to update model initialization, training loop, and loss calculation for improved performance and stability

--- components.py
+++ components.py
@@ -1,223 +1,192 @@
+import torch.nn as nn
 import torch
-import math
-import tqdm
+import torch.nn.functional as F
+from einops import rearrange
+import numpy as np
 
-class ConvolutionalBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, time_encoding_dim = 128, downsample = True):
-        super(ConvolutionalBlock, self).__init__()
-        self.downsample = downsample
-        if downsample:
-            self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size = 4, stride = 2, padding = 1)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class UNet_Tranformer(nn.Module):
+    def __init__(
+            self, 
+            marginal_prob_std, 
+            channels = [32, 64, 128, 256], 
+            embed_dim = 256,
+            text_dim = 256, 
+            nClass = 10
+        ):
+        super().__init__()
+        # Embedding layers
+        self.time_embed = nn.Sequential(
+            TimeProjection(embed_dim = embed_dim),
+            nn.Linear(embed_dim, embed_dim)
+        )
+        self.cond_embed = nn.Embedding(nClass, text_dim)
+
+        # Other model properties
+        self.act = nn.SiLU()
+        self.marginal_prob_std = marginal_prob_std
+        
+        # Encoding layers
+        self.conv1 = nn.Conv2d(1, channels[0], 3, stride = 1, bias = False)
+        self.dense1 = Dense(embed_dim, channels[0])
+        self.gnorm1 = nn.GroupNorm(4, num_channels = channels[0])
+
+        self.conv2 = nn.Conv2d(channels[0], channels[1], 3, stride = 2, bias = False)
+        self.dense2 = Dense(embed_dim, channels[1])
+        self.gnorm2 = nn.GroupNorm(32, num_channels = channels[1])
+
+        self.conv3 = nn.Conv2d(channels[1], channels[2], 3, stride=2, bias=False)
+        self.dense3 = Dense(embed_dim, channels[2])
+        self.gnorm3 = nn.GroupNorm(32, num_channels = channels[2])
+        self.attn3 = SpatialCrossAttention(channels[2], text_dim)
+
+        self.conv4 = nn.Conv2d(channels[2], channels[3], 3, stride=2, bias=False)
+        self.dense4 = Dense(embed_dim, channels[3])
+        self.gnorm4 = nn.GroupNorm(32, num_channels = channels[3])
+        self.attn4 = SpatialCrossAttention(channels[3], text_dim)
+
+        # Decoding layers
+        self.tconv4 = nn.ConvTranspose2d(channels[3], channels[2], 3, stride = 2, bias = False)
+        self.dense5 = Dense(embed_dim, channels[2])
+        self.tgnorm4 = nn.GroupNorm(32, num_channels = channels[2])
+
+        self.tconv3 = nn.ConvTranspose2d(channels[2], channels[1], 3, stride = 2, bias = False, output_padding = 1)
+        self.dense6 = Dense(embed_dim, channels[1])
+        self.tgnorm3 = nn.GroupNorm(32, num_channels = channels[1])
+
+        self.tconv2 = nn.ConvTranspose2d(channels[1], channels[0], 3, stride = 2, bias = False, output_padding = 1)
+        self.dense7 = Dense(embed_dim, channels[0])
+        self.tgnorm2 = nn.GroupNorm(32, num_channels = channels[0])
+
+        self.tconv1 = nn.ConvTranspose2d(channels[0], 1, 3, stride = 1)
+
+
+    def forward(self, x, t, y = None):
+        # Embed time and text
+        embed = self.act(self.time_embed(t))
+        y_embed = self.cond_embed(y).unsqueeze(1)
+        
+        # Encoding
+        h1 = self.act(self.gnorm1(self.conv1(x) + self.dense1(embed)))
+        h2 = self.act(self.gnorm2(self.conv2(h1) + self.dense2(embed)))
+        h3 = self.act(self.gnorm3(self.conv3(h2) + self.dense3(embed)))
+        h3 = self.attn3(h3, y_embed)
+        h4 = self.act(self.gnorm4(self.conv4(h3) + self.dense4(embed)))
+        h4 = self.attn4(h4, y_embed)
+
+        # Decoding
+        h = self.act(self.tgnorm4(self.tconv4(h4) + self.dense5(embed)))
+        h = self.act(self.tgnorm3(self.tconv3(h + h3) + self.dense6(embed)))
+        h = self.act(self.tgnorm2(self.tconv2(h + h2) + self.dense7(embed)))
+        h = self.tconv1(h + h1)
+
+        # Normalize predicted noise by std at time t
+        h = h / self.marginal_prob_std(t)[:, None, None, None]
+        return h
+    
+
+class Attention(nn.Module):
+    """
+    Implements a single-head attention mechanism. This class supports both self-attention
+    and cross-attention depending on the context provided.
+
+    Args:
+        embed_dim (int): The dimensionality of the embedding space.
+        hidden_dim (int): The dimensionality of the hidden states.
+        context_dim (int, optional): The dimensionality of the context for cross-attention. 
+                                     If None, self-attention is performed.
+        num_heads (int, optional): The number of attention heads. Default is 1.
+    """
+    def __init__(self, embed_dim, hidden_dim, context_dim = None):
+        super(Attention, self).__init__()
+        self.query = nn.Linear(hidden_dim, embed_dim, bias = False)
+        if context_dim is None:
+            self.self_attn = True
+            self.key = nn.Linear(hidden_dim, embed_dim, bias = False)
+            self.value = nn.Linear(hidden_dim, hidden_dim, bias = False)
         else:
-            self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size = 3, padding = 1)
-        self.gn = torch.nn.GroupNorm(1, out_channels)
-        self.time_scale = torch.nn.Linear(time_encoding_dim, out_channels)
-        self.time_shift = torch.nn.Linear(time_encoding_dim, out_channels)
+            self.self_attn = False
+            self.key = nn.Linear(context_dim, embed_dim, bias = False)
+            self.value = nn.Linear(context_dim, hidden_dim, bias = False)
 
-        self.activation = torch.nn.SiLU()
-
-    def forward(self, x, time_encoding):
-        time_scale = self.time_scale(time_encoding).unsqueeze(-1).unsqueeze(-1)
-        time_shift = self.time_shift(time_encoding).unsqueeze(-1).unsqueeze(-1)
-
-        time_scale = self.activation(time_scale)
-        time_shift = self.activation(time_shift)
-
-
-        
-        x = self.conv(x)
-        
-        x = self.activation(x)
-        x = self.gn(x)
-
-        x = x * time_scale + time_shift
-
-        return x
-    
-
-class ConvolutionalBlockTranspose(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, time_encoding_dim = 128, upsample = True, skip_connection_channels = 0):
-        super(ConvolutionalBlockTranspose, self).__init__()
-        self.upsample = upsample
-        if upsample:
-            self.conv = torch.nn.ConvTranspose2d(
-                in_channels + skip_connection_channels, 
-                out_channels, 
-                kernel_size = 4,
-                stride = 2, 
-                padding = 1
-                )
+    def forward(self, tokens, context = None):
+        if self.self_attn:
+            Q, K, V = self.query(tokens), self.key(tokens), self.value(tokens)
         else:
-            self.conv = torch.nn.ConvTranspose2d(
-                in_channels + skip_connection_channels, 
-                out_channels, 
-                kernel_size = 3, 
-                padding = 1
-                )
-        self.gn = torch.nn.GroupNorm(1, out_channels)
-        self.time_scale = torch.nn.Linear(time_encoding_dim, out_channels)
-        self.time_shift = torch.nn.Linear(time_encoding_dim, out_channels)
-
-        self.activation = torch.nn.SiLU()
-
-
-    def forward(self, x, time_encoding, skip_connection):
-
-        time_scale = self.time_scale(time_encoding).unsqueeze(-1).unsqueeze(-1)
-        time_shift = self.time_shift(time_encoding).unsqueeze(-1).unsqueeze(-1)
-
-        time_scale = self.activation(time_scale)
-        time_shift = self.activation(time_shift)
-
-        x = torch.cat([x, skip_connection], dim = 1)
-
+            Q, K, V = self.query(tokens), self.key(context), self.value(context)
         
-        x = self.conv(x)
-        x = self.activation(x)
-        x = self.gn(x)
+        scoremats = torch.einsum('bth,bsh->bts', Q, K)
+        attnmats = F.softmax(scoremats, dim=1)
+        ctx_vecs = torch.einsum("bts,bsh->bth", attnmats, V)
+        return ctx_vecs
 
-        x = x * time_scale + time_shift
+class TransformerBlock(nn.Module):
+    """
+    Implements a Transformer block that includes self-attention, cross-attention, 
+    and a feed-forward network with normalization layers.
 
-        return x
-    
-    
-class MNISTAutoencoder(torch.nn.Module):
-    def __init__(self, latent_dim, time_encoding_dim = 4):
-        super(MNISTAutoencoder, self).__init__()
-        
-        self.encoder_conv = torch.nn.ModuleList([
-            ConvolutionalBlock(1, 32, time_encoding_dim),
-            ConvolutionalBlock(32, 64, time_encoding_dim),
-            #ConvolutionalBlock(64, 128, time_encoding_dim, downsample = False),
-        ])
-
-        self.dense = torch.nn.Sequential(
-            torch.nn.Flatten(),
-            torch.nn.SiLU(),
-            torch.nn.Linear(64 * 7 * 7, latent_dim),
-            torch.nn.SiLU(),
-            torch.nn.Linear(latent_dim, 64 * 7 * 7),
-            torch.nn.SiLU(),
-            torch.nn.Unflatten(1, (64, 7, 7))
+    Args:
+        hidden_dim (int): The dimensionality of the hidden states.
+        context_dim (int): The dimensionality of the context for cross-attention.
+    """
+    def __init__(self, hidden_dim, context_dim):
+        super(TransformerBlock, self).__init__()
+        self.attn_self = Attention(hidden_dim, hidden_dim)
+        self.attn_cross = Attention(hidden_dim, hidden_dim, context_dim)
+        self.norm1 = nn.LayerNorm(hidden_dim)
+        self.norm2 = nn.LayerNorm(hidden_dim)
+        self.norm3 = nn.LayerNorm(hidden_dim)
+        self.ffn = nn.Sequential(
+            nn.Linear(hidden_dim, 4 * hidden_dim),
+            nn.GELU(),
+            nn.Linear(4 * hidden_dim, hidden_dim),
+            nn.GELU()
         )
 
+    def forward(self, x, context = None):
+        x = self.attn_self(self.norm1(x)) + x
+        x = self.attn_cross(self.norm2(x), context = context) + x
+        x = self.ffn(self.norm3(x)) + x
+        return x
 
-        self.decoder_conv = torch.nn.ModuleList([
-            #ConvolutionalBlockTranspose(128, 64, time_encoding_dim, upsample = False, skip_connection_channels = 128),
-            ConvolutionalBlockTranspose(64, 32, time_encoding_dim, skip_connection_channels = 64),
-            ConvolutionalBlockTranspose(32, 1, time_encoding_dim, skip_connection_channels = 32)
+class SpatialCrossAttention(nn.Module):
+    """
+    Implements a Spatial Cross Attention that applies a Transformer block to spatial data, 
+    typically images. This allows spatial interactions within the Transformer architecture.
 
-        ])
+    Args:
+        hidden_dim (int): The dimensionality of the hidden states.
+        context_dim (int): The dimensionality of the context for cross-attention.
+    """
+    def __init__(self, hidden_dim, context_dim):
+        super(SpatialCrossAttention, self).__init__()
+        self.transformer = TransformerBlock(hidden_dim, context_dim)
 
-        self.latent_dim = latent_dim
-        self.time_encoding_dim = time_encoding_dim
+    def forward(self, x, context = None):
+        b, c, h, w = x.shape
+        x_in = x
+        x = rearrange(x, "b c h w -> b (h w) c")
+        x = self.transformer(x, context)
+        x = rearrange(x, 'b (h w) c -> b c h w', h = h, w = w)
+        return x + x_in
 
-        self.time_mlp = torch.nn.Sequential(
-            torch.nn.Linear(time_encoding_dim, time_encoding_dim * 4),
-            torch.nn.SiLU(),
-            torch.nn.Linear(time_encoding_dim * 4, time_encoding_dim),
-            torch.nn.SiLU(),
-        )
-
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.to(self.device)
-
-    def time_encoding(self, t):
-        sin = torch.cat([torch.sin(2 * math.pi * t * mul / self.steps).unsqueeze(1) for mul in range(1, self.time_encoding_dim // 2 + 1)], dim = 1)
-        cos = torch.cat([torch.cos(2 * math.pi * t * mul / self.steps).unsqueeze(1) for mul in range(1, self.time_encoding_dim // 2 + 1)], dim = 1)
-        
-        out = torch.cat([sin, cos], dim = 1)
-        return out
+class TimeProjection(nn.Module):
+    def __init__(self, embed_dim, time_steps = 1000):
+        super().__init__()
+        self.embed_dim = embed_dim
+        self.proj = nn.Embedding(time_steps, embed_dim)
     
-    def forward(self, x, t):
-        time_encoding = self.time_encoding(t)
-        time_encoding = self.time_mlp(time_encoding)
+    def forward(self, t):
+        return self.proj(t)
 
-        skip_connections = []
-        for idx, conv in enumerate(self.encoder_conv):
-            x = conv(x, time_encoding)
-            skip_connections.append(x.clone())
-
-        x = self.dense(x)
-
-        for idx, conv in enumerate(self.decoder_conv):
-            x = conv(x, time_encoding, skip_connections[-(idx + 1)])
-
-        return x
+class Dense(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.dense = nn.Linear(input_dim, output_dim)
+    
+    def forward(self, x):
+        return self.dense(x)[..., None, None]
 
 
-
-class MNISTDiffusionAutoencoder(MNISTAutoencoder):
-    def __init__(self, latent_dim = 2, time_encoding_dim = 2, beta_min = 0.001, beta_max = 0.02, steps = 1000):
-        super(MNISTDiffusionAutoencoder, self).__init__(latent_dim = latent_dim, time_encoding_dim = time_encoding_dim)
-
-        self.steps = steps
-
-        self.beta = torch.linspace(beta_min, beta_max, steps)
-        self.alpha = 1 - self.beta
-
-        self.alpha_hat = torch.cumprod(self.alpha, dim = 0)
-
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
-
-        self.loss_fn = torch.nn.MSELoss()
-
-        self.beta = self.beta.to(self.device)
-        self.alpha_hat = self.alpha_hat.to(self.device)
-        self.alpha = self.alpha.to(self.device)
-
-        self.reconstructing_noise = False
-
-
-    def train_step(self, x, t = None):
-        x = x.to(self.device)
-        if t is None:
-            t = torch.randint(0, len(self.alpha) - 1, (x.size(0),)).long()
-        noise = torch.randn_like(x).to(self.device)
-        t = t.to(self.device)
-        x_t = torch.sqrt(self.alpha_hat[t].reshape(-1, 1, 1, 1)) * x + torch.sqrt(1 - self.alpha_hat[t].reshape(-1, 1, 1, 1)) * noise
-        output = self(x_t, t)
-        if self.reconstructing_noise:
-            loss = self.loss_fn(output, noise)
-        else:
-            loss = self.loss_fn(output, x)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        return loss.item()
-
-
-    def sample(self, x = None, T = None):
-        if x is None:
-            x = torch.randn((1, 1, 28, 28)).to(self.device)
-        if T is None:
-            T = self.steps - 1
-        print("Sampling...")
-        for t in tqdm.tqdm(range(T - 1, 0, -1)):
-            t = torch.tensor([t]).to(self.device)
-            if self.reconstructing_noise:
-                recon_noise = self(x, t)
-            else:
-                recon_x = self(x, t)
-                recon_noise  = x - recon_x
-            mu = 1 / torch.sqrt(self.alpha[t]) * (x - (1 - self.alpha[t]) / torch.sqrt(1 - self.alpha_hat[t] + 1e-5) * recon_noise)
-            sigma = torch.sqrt(self.beta[t])
-            x = mu + sigma * torch.randn_like(x)
-            #x = torch.clamp(x, -3, 3)
-        if self.reconstructing_noise:
-            recon_noise = self(x, torch.tensor([0]).to(self.device)).to(self.device)
-        else:
-            recon_x = self(x, torch.tensor([0]).to(self.device)).to(self.device)
-            recon_noise  = x - recon_x
-        x = 1 / torch.sqrt(self.alpha[0]) * (x - (1 - self.alpha[0]) / torch.sqrt(1 - self.alpha_hat[0] + 1e-5) * recon_noise)
-        #x = torch.clamp(x, -3, 3)
-        return x
-
-        
-
-if __name__ == "__main__":
-    # Test the MNISTDiffusionAutoencoder class
-    model = MNISTDiffusionAutoencoder(latent_dim=16, time_encoding_dim = 2, steps = 1000)
-    x = torch.randn((3, 1, 28, 28)).to(model.device)
-    model.train()
-    model.train_step(x)
--- main.py
+++ main.py
@@ -1,54 +1,60 @@
-from components import MNISTDiffusionAutoencoder
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, Subset
-from tqdm import tqdm
 import torch
-import utils
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+from tqdm import tqdm
+from components import UNet_Tranformer
 
+# === 1. Parametry ===
+batch_size = 128
+num_epochs = 3
+learning_rate = 2e-4
+timesteps = 1000
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# === 2. Rozkład szumu (marginal_prob_std) ===
+def marginal_prob_std(t, sigma=25.):
+    return torch.sqrt((sigma ** (2 * t) - 1.) / (2. * torch.log(torch.tensor(sigma))))
 
-def main():
-    torch.manual_seed(42)
+# === 3. Ładowanie danych ===
+transform = transforms.Compose([
+    transforms.ToTensor(),  # skaluje do [0, 1]
+    lambda x: x * 2. - 1.   # skaluje do [-1, 1]
+])
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,)),
-        ])
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+# === 4. Inicjalizacja modelu ===
+model = UNet_Tranformer(marginal_prob_std=marginal_prob_std).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# === 5. Funkcja treningowa ===
+for epoch in range(num_epochs):
+    model.train()
+    epoch_loss = 0.
     
-    epochs = 20
+    for x, y in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+        x, y = x.to(device), y.to(device)
+
+        # 5.1 Losowy krok czasu dla każdego elementu w batchu
+        t = torch.randint(low=1, high=timesteps, size=(x.size(0),), device=device)
+        t_norm = t.float() / timesteps  # skaluje t do [0, 1]
+
+        # 5.2 Szum i zaszumiony obraz
+        z = torch.randn_like(x)
+        std = marginal_prob_std(t_norm).view(-1, 1, 1, 1)
+        x_noisy = x + std * z
+
+        # 5.3 Predykcja szumu
+        z_pred = model(x_noisy, t, y=y)
+
+        # 5.4 MSE loss między prawdziwym a przewidzianym szumem
+        loss = F.mse_loss(z_pred, z)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
     
-    train_dataset = datasets.MNIST(root='./data', train = True, download = True, transform = transform)
-
-    indices = [i for i, (_, label) in enumerate(train_dataset) if label == 7]
-
-    #Utwórz podzbiór tylko z siódemkami
-    subset = Subset(train_dataset, indices)
-
-
-    
-    train_loader = DataLoader(subset, batch_size = 64, shuffle = True)
-    model = MNISTDiffusionAutoencoder(latent_dim = 2, time_encoding_dim = 16, steps = 10)
-    for epoch in range(epochs):
-        model.train()
-        loss = 0
-        for x, _ in tqdm(train_loader):
-            loss += model.train_step(x)
-        print(f"Epoch {epoch + 1}, Loss: {loss}")
-
-    model.eval()
-    with torch.no_grad():
-        for i in range(10):
-            recon = model.sample()
-            #print(recon)
-            utils.display(recon)
-
-
-    
-
-
-
-if __name__ == "__main__":
-    main()
-
-
-
+    print(f"Epoch {epoch+1}, Loss: {epoch_loss / len(train_loader):.4f}")
